55d5b14cb979693c5cdcfc742e61c8d6a1c341e5
s1080143 <Abishek.Subramanian@scotiabank.com>
2019-05-03 12:16:56 -0400

    commit in master

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World12!");
-        System.out.println("Master commit");
-        System.out.println("Master commit 2");
-        System.out.println("Master commit 3");
+
     }
 }
