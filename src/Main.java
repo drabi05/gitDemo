6001c8142c6d5d38b818ab98ae74c9067eec6213
s1080143 <Abishek.Subramanian@scotiabank.com>
2019-05-03 13:17:17 -0400

    commit in branch

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Drabi 1");
-        System.out.println("Drabi 2");
+        System.out.println("Drabi 12");
 
     }
 }
